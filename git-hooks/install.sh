9141dced71f155a38906933143d043cc148ebcba
Edward Skalibog <skalibog@gmail.com>
2019-07-29 10:33:25 +0300

    rework install script

--- git-hooks/install.sh
+++ git-hooks/install.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env zsh
 
 GIT_DIR=$(git rev-parse --git-dir)
+SCRIPT_PATH=`dirname "$0"`
+
+# set git ignore to scripts
+
+echo "/scripts/" >> .gitignore
 
 echo "Installing hooks..."
 # this command creates symlink to our pre-commit script
-ln -s ../../scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
-ln -s ../../scripts/pre-push.bash $GIT_DIR/hooks/pre-push
+cp -R $SCRIPT_PATH/run/ ./scripts/
+cp $SCRIPT_PATH/pre-commit.sh $GIT_DIR/hooks/pre-commit
+cp $SCRIPT_PATH/pre-push.sh $GIT_DIR/hooks/pre-push
 echo "Done!"
